060e461f5db6b04c3fe5c90f77d55b63a2ae8013
xhayper <46065349+xhayper@users.noreply.github.com>
2021-12-31 20:17:26 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# RandomCardAttribute
+
+A mod for Inscryption where it randomize your card on battle.
+
+## Installation (game, automated)
+
+This is the recommended way to install this plugin on the game.
+
+1. Download and install [Thunderstore Mod Manager](https://www.overwolf.com/app/Thunderstore-Thunderstore_Mod_Manager) or [r2modman](https://timberborn.thunderstore.io/package/ebkr/r2modman/)
+2. Click **Install with Mod Manager** button on top of the page
+3. Run the game via the mod manager
+
+## Installation (manual)
+
+If you are installing this manually, do the following
+
+1. Extract the archive into a folder. Do not extract into the game folder.
+2. Move the _RandomCardAttribute.dll_ into the _BepInEx/plugins_ folder.
+3. Run the game.
+
+## Configuration
+
+You can configurate the behaviour of this mod by doing the following
+
+1. Head to _BepInEx/config_
+2. Edit the _io.github.xhayper.randomcardattribute.cfg_ file
+3. Save and re-open the game.
+
+## TODO
+
+|                Task                 | Status |                     Note                      |
+| :---------------------------------: | :----: | :-------------------------------------------: |
+| Randomize Ability / Special Ability |  null  | Please send help, I don't know how to do this |
+
+## Credits
+
+- [InscryptionAPI](https://github.com/ScottWilson0903/InscryptionAPI) - Help me learn how to make a mod for this game.
+- [BepInExPack Inscryption](https://inscryption.thunderstore.io/package/BepInEx/BepInExPack_Inscryption/) - README . md
---
+++ Utility.cs
@@ -0,0 +1,45 @@
+using APIPlugin;
+using DiskCardGame;
+using System.Collections.Generic;
+
+namespace RandomCardAttribute
+{
+    public class Utility
+    {
+        public static Ability randomAbility()
+        {
+            List<Ability> values = AbilitiesUtil.GetAbilities(true);
+            values.AddRange(AbilitiesUtil.GetAbilities(false));
+            Ability randAbility = values[UnityEngine.Random.RandomRangeInt(0, values.Count - 1)];
+            if (randAbility == Ability.None || randAbility == Ability.NUM_ABILITIES) return randomAbility();
+            return randAbility;
+        }
+
+        public static List<Ability> randomAbilityList(int listSize = 3)
+        {
+            List<Ability> abilities = new List<Ability>();
+            for (int i = 0; i < listSize; i++)
+            {
+                Ability randAbility = randomAbility();
+                if (abilities.Contains(randAbility)) continue;
+                abilities.Add(randAbility);
+            }
+            return abilities;
+        }
+
+        public static CardInfo randomCardInfo(CardInfo cardInfo)
+        {
+            Plugin.Log.LogDebug($"Randomizing card: {cardInfo.name}");
+            if (!Plugin.configRandomizeSquirrelCard.Value && cardInfo.name == "Squirrel") return cardInfo;
+            int costType = UnityEngine.Random.RandomRangeInt(0, 2);
+            return new CustomCard(cardInfo.name)
+            {
+                baseAttack = UnityEngine.Random.RandomRangeInt(Plugin.configMinAttack.Value, Plugin.configMaxAttack.Value),
+                baseHealth = UnityEngine.Random.RandomRangeInt(Plugin.configMinHealth.Value, Plugin.configMaxHealth.Value),
+                cost = Plugin.configRandomizeAllCost.Value || costType == 0 ? UnityEngine.Random.RandomRangeInt(Plugin.configMinCost.Value, Plugin.configMaxCost.Value) : 0,
+                bonesCost = Plugin.configRandomizeAllCost.Value || costType == 1 ? UnityEngine.Random.RandomRangeInt(Plugin.configMinCost.Value, Plugin.configMaxCost.Value) : 0,
+                energyCost = Plugin.configRandomizeAllCost.Value || costType == 2 ? UnityEngine.Random.RandomRangeInt(Plugin.configMinCost.Value, Plugin.configMaxCost.Value) : 0,
+            }.AdjustCard(cardInfo);
+        }
+    }
+}
